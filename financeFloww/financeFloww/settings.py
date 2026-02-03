d6e71119b58f3e5a1b021f2e3516ea73ca7b0a4a
Vishwajeet-SDE <sde.vishwajeet@gmail.com>
2026-02-03 23:34:51 +0530

    3rd commit

--- financeFloww/financeFloww/settings.py
+++ financeFloww/financeFloww/settings.py
@@ -87,8 +87,12 @@ WSGI_APPLICATION = 'financeFloww.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'floww',
+        'USER': 'postgres',
+        'PASSWORD': '@Bcaf1VSarkar',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
